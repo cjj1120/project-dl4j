27fb57eb70dde11d3f84a134afc004464e712256
Your Name <cjj8168@gmail.com>
2021-11-24 21:44:46 +0800

    add

---
+++ src/main/java/Helper.java
@@ -0,0 +1,44 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+public class Helper {
+        public static String getPropValues(String property)throws IOException {
+            InputStream inputStream = null;
+            String value = null;
+
+            try{
+                Properties prop = new Properties();
+                String propFileName = "myconfig.properties";
+
+                inputStream = Helper.class.getClassLoader().getResourceAsStream(propFileName);
+
+                if (inputStream != null) {
+                    prop.load(inputStream);
+                } else {
+                    throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+                }
+
+                value = prop.getProperty(property);
+            } catch (Exception  e) {
+                e.printStackTrace();
+            } finally {
+                inputStream.close();
+            }
+
+            return value;
+        }
+
+        public static String getCheckSum(String filePath) {
+            String hashValue = "";
+            try (InputStream is = Files.newInputStream(Paths.get(filePath))) {
+                hashValue = org.apache.commons.codec.digest.DigestUtils.md5Hex(is);
+            } catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+            return hashValue;
+        }
+}
